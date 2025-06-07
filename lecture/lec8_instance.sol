933edf9f931145b564823b22cd77f6f3fac43602
Guitarjourneyman <ayedh0421@gmail.com>
2025-06-07 23:43:52 +0800

    Blockchain_Solidity_1st

---
+++ lecture/lec8_instance.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL - 30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract A{
+
+    string public name;
+    uint256 public age;
+
+    constructor(string memory _name, uint256 _age){
+        name = _name;
+        age = _age;
+    }
+    function change(string memory _name, uint256 _age)public  {
+        name = _name;
+        age = _age;
+    }
+
+
+}
+
+contract B{
+    A instance_a = new A("Tanay", 18);
+
+    function get() public view returns(string memory, uint256){
+        return(instance_a.name(), instance_a.age());
+    }
+
+    function change(string memory _name, uint256 _age)public{
+        instance_a.change(_name, _age);
+    }
+}
